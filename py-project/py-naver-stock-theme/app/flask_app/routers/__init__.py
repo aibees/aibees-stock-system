fad52d8b36647960431b45e0fb7f175a596b3bf1
aibees <qwt0326@gmail.com>
2026-08-07 01:29:10 +0900

    monorepo 조정

--- py-project/py-naver-stock-theme/app/flask_app/routers/__init__.py
+++ py-project/py-naver-stock-theme/app/flask_app/routers/__init__.py
@@ -10,7 +10,7 @@ from app.flask_app.routers.router_anthropic import anthropic_bp
 from app.flask_app.routers.router_sell_request import sell_request_bp
 from app.flask_app.routers.router_user_options import user_options_bp
 from app.flask_app.routers.router_strategy import strategy_bp
-from app.flask_app.routers.router_upbit import upbit_bp
+# from app.flask_app.routers.router_upbit import upbit_bp
 from app.flask_app.routers.router_account_trade import account_trade_bp
 
 def register_blueprints(app: Flask):
@@ -31,5 +31,5 @@ def register_blueprints(app: Flask):
     app.register_blueprint(sell_request_bp, url_prefix="/api/v1")
     app.register_blueprint(user_options_bp, url_prefix="/api/v1")
     app.register_blueprint(strategy_bp, url_prefix="/api/v1/strategy")
-    app.register_blueprint(upbit_bp, url_prefix="/api/v1/upbit")
+    # app.register_blueprint(upbit_bp, url_prefix="/api/v1/upbit")
     app.register_blueprint(account_trade_bp, url_prefix="/api/v1/users")
